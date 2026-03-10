b1f3ad7e45cebeb2b8f51cf48f7319c794b58795
Raj Nangare <rajjagannathnangare@gmail.com>
2026-03-10 23:53:55 +0530

    Completion of Tanh Activation Function

--- Activation_Function/Activation.py
+++ Activation_Function/Activation.py
@@ -6,3 +6,10 @@ class Activation_Function:
 
     def sigmoid(self, input):
         return 1 / (1 + math.exp(-input))
+    
+    def linear(self, input):
+        return input
+    
+    def tanh(self, input):
+        return (math.exp(input) - math.exp(-input)) / (math.exp(input) + math.exp(-input))
+    
